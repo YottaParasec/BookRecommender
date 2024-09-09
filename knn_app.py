c7de20b8d8c34e052907b1b21893d9a316735b27
Mohammed Bookwala <mdbk2004@gmail.com>
2024-09-09 09:40:19 +0530

    Update knn_app.py

--- knn_app.py
+++ knn_app.py
@@ -147,7 +147,7 @@ st.write('')
 st.write('')
 
 # If input and slider values are valid, display results
-if button or user_input_:
+if button:
     st.success("Your Book Recommendations:")
     with st.spinner('Finding the best books for you...'):
         results = recommender(user_input_, neighbors, recommendation_type_value)  # Call recommender function
